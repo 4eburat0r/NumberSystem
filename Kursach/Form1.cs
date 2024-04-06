f92c06d905f8e56be96a9a71a96124c84377e138
4eburator <nikitafedorenko51@gmail.com>
2024-04-06 23:52:33 +0300

    Добавьте файлы проекта.

---
+++ Kursach/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kursach
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //переменная для получения исходной системы счисления (original number system)
+            string origNumSys = comboBox1.SelectedItem.ToString();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //переменная для получения требуемой системы счисления (required number system)
+            string reqNumSys = comboBox2.SelectedItem.ToString();
+        }
+    }
+}
