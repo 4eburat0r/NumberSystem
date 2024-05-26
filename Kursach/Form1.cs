8e5ea8bc69645f9839035c7a94df32f399c79c9f
4eburator <nikitafedorenko51@gmail.com>
2024-05-27 00:17:51 +0300

    add func

--- Kursach/Form1.cs
+++ Kursach/Form1.cs
@@ -21,27 +21,26 @@ namespace Kursach
         {
             if (rbBinarySys1.Checked == true)
             {
-                if (!(e.KeyChar >= '0' && e.KeyChar <= '1' || (int)e.KeyChar == 8 || ((e.KeyChar == 45) && (textBox.Text.Length == 0)) || (int)e.KeyChar == 44))
+                if (!(e.KeyChar >= '0' && e.KeyChar <= '1' || (int)e.KeyChar == 8 ))
                     return false;
                 else return true;
             }
             if (rbOctalSys1.Checked == true)
             {
-                if (!(e.KeyChar >= '0' && e.KeyChar <= '7' || (int)e.KeyChar == 8 || ((e.KeyChar == 45) && (textBox.Text.Length == 0)) || (int)e.KeyChar == 44))
+                if (!(e.KeyChar >= '0' && e.KeyChar <= '7' || (int)e.KeyChar == 8 ))
                     return false;
                 else return true;
             }
             if (rbDecimalSys1.Checked == true)
             {
-                if (!(e.KeyChar >= '0' && e.KeyChar <= '9' || (int)e.KeyChar == 8 || ((e.KeyChar == 45) && (textBox.Text.Length == 0)) || (int)e.KeyChar == 44))
+                if (!(e.KeyChar >= '0' && e.KeyChar <= '9' || (int)e.KeyChar == 8 ))
                     return false;
                 else return true;
             }
             if (rbHexaSys1.Checked == true)
             {
                 if (!(e.KeyChar >= '0' && e.KeyChar <= '9' || (int)e.KeyChar == 8
-                || ((int)e.KeyChar >= 65 && (int)e.KeyChar <= 70) || ((int)e.KeyChar >= 97 && (int)e.KeyChar <= 102) || 
-                ((e.KeyChar == 45) && (textBox.Text.Length == 0)) || (int)e.KeyChar == 44))
+                || ((int)e.KeyChar >= 65 && (int)e.KeyChar <= 70) || ((int)e.KeyChar >= 97 && (int)e.KeyChar <= 102)))
                     return false;
                 else return true;
             }
@@ -52,19 +51,19 @@ namespace Kursach
         {
             if (comboBox1.SelectedIndex == 0)
             {
-                if (!(e.KeyChar >= '0' && e.KeyChar <= '1' || (int)e.KeyChar == 8 || ((e.KeyChar == 45) && (textBox.Text.Length == 0)) || (int)e.KeyChar == 44))
+                if (!(e.KeyChar >= '0' && e.KeyChar <= '1' || (int)e.KeyChar == 8 ))
                     return false;
                 else return true;
             }
             if (comboBox1.SelectedIndex == 1)
             {
-                if (!(e.KeyChar >= '0' && e.KeyChar <= '7' || (int)e.KeyChar == 8 || ((e.KeyChar == 45) && (textBox.Text.Length == 0)) || (int)e.KeyChar == 44))
+                if (!(e.KeyChar >= '0' && e.KeyChar <= '7' || (int)e.KeyChar == 8 ))
                     return false;
                 else return true;
             }
             if (comboBox1.SelectedIndex == 2)
             {
-                if (!(e.KeyChar >= '0' && e.KeyChar <= '9' || (int)e.KeyChar == 8 || ((e.KeyChar == 45) && (textBox.Text.Length == 0)) || (int)e.KeyChar == 44))
+                if (!(e.KeyChar >= '0' && e.KeyChar <= '9' || (int)e.KeyChar == 8 ))
                     return false;
                 else return true;
             }
@@ -72,7 +71,7 @@ namespace Kursach
             {
                 if (!(e.KeyChar >= '0' && e.KeyChar <= '9' || (int)e.KeyChar == 8
                 || ((int)e.KeyChar >= 65 && (int)e.KeyChar <= 70) || ((int)e.KeyChar >= 97 && (int)e.KeyChar <= 102)
-                || ((e.KeyChar == 45) && (textBox.Text.Length == 0)) || (int)e.KeyChar == 44))
+                ))
                     return false;
                 else return true;
             }
@@ -98,25 +97,58 @@ namespace Kursach
         {
             textBox5.ReadOnly = true;
         }
-        double binaryToDecimal(TextBox textbox)
+        const int base10 = 10;
+        char[] cHexa = new char[] { 'A', 'B', 'C', 'D', 'E', 'F' };
+        int[] iHexaNumeric = new int[] { 10, 11, 12, 13, 14, 15 };
+        int[] iHexaIndices = new int[] { 0, 1, 2, 3, 4, 5 };
+        const int asciiDiff = 48;
+        string DecimalToBase(int iDec, int numbase)
         {
-            double binaryNum = Convert.ToDouble(textbox);
-            double sum;
-            for(int i = 0; i < sizeof(binaryNum); i++)
+            string strBin = "";
+            int[] result = new int[32];
+            int MaxBit = 32;
+            for (; iDec > 0; iDec /= numbase)
             {
-
+                int rem = iDec % numbase;
+                result[--MaxBit] = rem;
             }
+            for (int i = 0; i < result.Length; i++)
+                if ((int)result.GetValue(i) >= base10)
+                    strBin += cHexa[(int)result.GetValue(i) % base10];
+                else
+                    strBin += result.GetValue(i);
+            strBin = strBin.TrimStart(new char[] { '0' });
+            return strBin;
+        }
+        int BaseToDecimal(string sBase, int numbase)
+        {
+            int dec = 0;
+            int b;
+            int iProduct = 1;
+            string sHexa = "";
+            if (numbase > base10)
+                for (int i = 0; i < cHexa.Length; i++)
+                    sHexa += cHexa.GetValue(i).ToString();
+            for (int i = sBase.Length - 1; i >= 0; i--, iProduct *= numbase)
+            {
+                string sValue = sBase[i].ToString();
+                if (sValue.IndexOfAny(cHexa) >= 0)
+                    b = iHexaNumeric[sHexa.IndexOf(sBase[i])];
+                else
+                    b = (int)sBase[i] - asciiDiff;
+                dec += (b * iProduct);
+            }
+            return dec;
         }
         private void button1_Click(object sender, EventArgs e)
         {
             try
             {
-                //---ОПЕРАЦИИ ПЕРЕВОДА---
+                //---ОПЕРАЦИИ ПЕРЕВОДА--- 
                 //десятичная в двоичную и обратно
                 if (rbDecimalSys1.Checked == true && rbBinarySys2.Checked == true)
                 {
-                    int decimalValue = Convert.ToInt32(textBox1.Text, 10);
-                    textBox2.Text = Convert.ToString(decimalValue, 2);
+                    textBox2.Text = DecimalToBase(Int32.Parse(textBox1.Text), 2);
                 }
                 if (rbBinarySys1.Checked == true && rbDecimalSys2.Checked == true)
                 {
